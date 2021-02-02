adadeb3e2983687f07ab516e3706d5c528df82f0
Pasquale Cavallo <pasquale.cavallo@lyqid.com>
2021-02-03 00:29:44 +0100

    start

---
+++ src/main/java/com/lyqid/pbems/PbemsApplication.java
@@ -0,0 +1,13 @@
+package com.lyqid.pbems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyqid/pbems/PbemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyqid.pbems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
